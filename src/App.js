e139fc616446b871b454ef19a99086441a9864f9
MaSsoumeh <massoumehzarei@gmail.com>
2022-05-06 02:49:35 +0430

    make it responsive

--- src/App.js
+++ src/App.js
@@ -2,17 +2,48 @@ import AddSong from "./components/AddSong";
 import Header from "./components/Header";
 import SongList from "./components/SongList";
 import SongPlayer from "./components/SongPlayer";
-import { Grid } from "@mui/material";
+import { Grid, useMediaQuery } from "@mui/material";
 function App() {
+  const greaterThanMd = useMediaQuery((theme) => theme.breakpoints.up("md"));
+  const greaterThanSm = useMediaQuery((theme) => theme.breakpoints.up("sm"));
   return (
     <>
-      <Header />
-      <Grid sx={{ p: 10 }} container spacing={3}>
+      {greaterThanSm && <Header />}
+      <Grid
+        sx={{
+          padding: greaterThanSm ? "80px 40px" : "40px 40px",
+          background:
+            "linear-gradient(225deg, hsla(289, 100%, 16%, 1) 5%, hsla(305, 85%, 65%, 1) 100%)",
+        }}
+        container
+        spacing={3}
+      >
         <Grid item xs={12} md={7}>
           <AddSong />
           <SongList />
         </Grid>
-        <Grid item xs={12} md={5}>
+        <Grid
+          sx={
+            greaterThanMd
+              ? {
+                  position: "fixed",
+                  width: "100%",
+                  top: 70,
+                  right: 0,
+                  p: 2,
+                }
+              : {
+                  position: "fixed",
+                  width: "100%",
+                  bottom: 0,
+                  right: 0,
+                  padding: "0px !important",
+                }
+          }
+          item
+          xs={12}
+          md={5}
+        >
           <SongPlayer />
         </Grid>
       </Grid>
