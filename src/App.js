1f77f93f1ebcfcab65431d3aac7fec36758e0348
MaSsoumeh <massoumehzarei@gmail.com>
2022-05-05 18:42:15 +0430

    Customize theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import AddSong from "./components/AddSong";
+import Header from "./components/Header";
+import SongList from "./components/SongList";
+import SongPlayer from "./components/SongPlayer";
+import { Grid } from "@mui/material";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+      <Grid sx={{ p: 10 }} container spacing={3}>
+        <Grid item xs={12} md={7}>
+          <AddSong />
+          <SongList />
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <SongPlayer />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#C436FF",
+    },
+    secondary: { main: "#161234" },
+  },
+});
+export default theme;
