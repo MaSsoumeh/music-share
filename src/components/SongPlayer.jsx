2444d2100f712fdfa9d75edd6d534110e850b4d4
MaSsoumeh <massoumehzarei@gmail.com>
2022-05-05 18:43:50 +0430

    Create other base components

---
+++ src/components/SongPlayer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PlayList from "./PlayList";
+
+const SongPlayer = () => {
+  return (
+    <div>
+      SongPlayer
+      <PlayList />
+    </div>
+  );
+};
+
+export default SongPlayer;
