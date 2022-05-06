617886ce359438fe11fbd4108b19eb2fd7dd9906
MaSsoumeh <massoumehzarei@gmail.com>
2022-05-06 16:51:07 +0430

    add apolloProvider

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from "./App";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material";
 import theme from "./theme";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./graphql/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
